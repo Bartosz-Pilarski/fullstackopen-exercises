07e14fe218cf930c5a66854e484789d3c5e8949d
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2023-12-25 19:46:39 +0100

    More work on part1, modified ESLint

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p> Yeah okay buddy. boy. buddy {props.name}. buddy. boy. </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date();
+  console.log(now);
+
+  return (
+    <div>
+      <Hello 
+        name="boy"
+      />
+      <Hello 
+        name={"babl"+"oinga"}
+      />
     </div>
   )
 }
