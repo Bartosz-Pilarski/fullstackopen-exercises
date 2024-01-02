5712c30899e8528a267046d3a1aa6e8a1bf36381
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-01-03 00:04:48 +0100

    Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,6 +12,30 @@ const StatDisplay = ({text, stat}) => {
   )
 } 
 
+const Statistics = ({good, neutral, bad}) => {
+  const voteSum = good+neutral+bad;
+
+  const calculateAverageFeedback = () => {
+    const sum = (good*1)+(bad*-1)
+    console.log("Sum: ", sum, "Vote sum: ", voteSum);
+    return sum/voteSum
+  }
+
+  const calculatePositivePercentage = () => {return (good/voteSum)*100+"%"}
+
+  return (
+    <div>
+        <h1>statistics</h1>
+          <StatDisplay text="good" stat={good}/>
+          <StatDisplay text="neutral" stat={neutral}/>
+          <StatDisplay text="bad" stat={bad}/>
+          <StatDisplay text="all" stat={voteSum}/>
+          <StatDisplay text="average" stat={calculateAverageFeedback()}/>
+          <StatDisplay text="positive" stat={calculatePositivePercentage()}/>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -28,29 +52,13 @@ const App = () => {
     setBad(bad+1)
   }
 
-  const voteSum = good+neutral+bad;
-
-  const calculateAverageFeedback = () => {
-    const sum = (good*1)+(bad*-1)
-    console.log("Sum: ", sum, "Vote sum: ", voteSum);
-    return sum/voteSum
-  }
-
-  const calculatePositivePercentage = () => {return (good/voteSum)*100+"%"}
-
   return (
     <div>
       <h1>give feedback</h1>
         <Button text="good" onClick={handleGoodFeedback}/>
         <Button text="neutral" onClick={handleNeutralFeedback}/>
         <Button text="bad" onClick={handleBadFeedback}/>
-      <h1>statistics</h1>
-        <StatDisplay text="good" stat={good}/>
-        <StatDisplay text="neutral" stat={neutral}/>
-        <StatDisplay text="bad" stat={bad}/>
-        <StatDisplay text="all" stat={voteSum}/>
-        <StatDisplay text="average" stat={calculateAverageFeedback()}/>
-        <StatDisplay text="positive" stat={calculatePositivePercentage()}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
