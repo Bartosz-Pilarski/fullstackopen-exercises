337d3a1b905e1c02824940bd3d242b45c0212151
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-01-07 20:50:19 +0100

    Exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,46 +1,6 @@
-const Header = ({coursename}) => {
-  return (
-    <h1> {coursename} </h1>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map((part) => (<Part key={part.id} part={part}/>))}
-    </div>
-  )
-}
-
-const Total = ({parts}) => {
-  return (
-    <p>
-      Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
-    </p>
-  )
-}
-
-const Course = ({courses}) => {
-  return(
-    <div>
-    {courses.map((course) => {return (
-        <div key={course.id}>
-          <Header coursename={course.name}/>
-          <Content parts={course.parts}/>
-          <Total parts={course.parts}/>
-        </div>
-    )})}
-    </div>
-  )
-}
+//This seems to throw a JSON source map warning for react devtools, however the site still works fine and I have no idea what could be causing it specifically.
+//It doesn't reoccur after vite's hot reloads either, only after a full site refresh
+import Course from './components/Course.jsx'
 
 const App = () => {
   const courses = [
