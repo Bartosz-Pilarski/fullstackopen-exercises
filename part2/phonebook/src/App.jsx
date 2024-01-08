5f22e3abc6b197dee28dfa4e3908f9e911137c55
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-01-08 22:18:15 +0100

    Exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+const Person = ({person}) => (<p> {person.name} <br /> {person.number} </p>) 
+
+const Contacts = ({persons, searchInput}) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      <div>
+        {
+        searchInput !== '' 
+        ? persons.map((person) => {
+          if(searchInput.toLowerCase() === person.name.substring(0, searchInput.length).toLowerCase()) return (<Person key={person.id} person={person}/>)
+        })
+        : persons.map((person) => <Person key={person.id} person={person}/>)
+        }
+      </div>
+    </div>
+  )
+}
+
+const ContactForm = ({persons, setPersons}) => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [searchInput, setSearchInput] = useState('')
+
+  const handleNewNameChange = (event) => { setNewName(event.target.value) }
+  const handleNewNumberChange = (event) => { setNewNumber(event.target.value) }
 
   const handlePersonSubmit = (event) => {
     event.preventDefault();
@@ -19,8 +34,23 @@ const App = () => {
     setPersons(persons.concat({name: newName, number: newNumber, id:(persons.length+1)}))
   }
 
-  const handleNewNameChange = (event) => { setNewName(event.target.value) }
-  const handleNewNumberChange = (event) => { setNewNumber(event.target.value) }
+  return (
+    <form onSubmit={handlePersonSubmit}>
+      <div>
+        name: <input value={newName} onChange={handleNewNameChange}/>
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNewNumberChange}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+   </form>
+  )
+}
+
+const Search = ({searchInput, setSearchInput}) => {
+
   const handleSearchInput = (event) => {
     const currentInput = event.target.value 
     setSearchInput(currentInput) 
@@ -28,35 +58,30 @@ const App = () => {
 
   return (
     <div>
-      <h1>Phonebook</h1>
       <h2> Search </h2>
-        <div>
-          search: <input value={searchInput} onChange={handleSearchInput}/>
-        </div>
-      <h2> Contacts </h2>
       <div>
-        {
-        //This could probably be handled better by a separate contacts component.
-        searchInput !== '' 
-        ? persons.map((person) => {
-          if(searchInput.toLowerCase() === person.name.substring(0, searchInput.length).toLowerCase()) return (<p key={person.id}> {person.name} <br /> {person.number} </p>)
-        })
-        : persons.map((person) => (<p key={person.id}> {person.name} <br /> {person.number} </p>))
-        }
+        search: <input value={searchInput} onChange={handleSearchInput}/>
       </div>
-      <form onSubmit={handlePersonSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNewNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [searchInput, setSearchInput] = useState('')
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Search searchInput={searchInput} setSearchInput={setSearchInput}/>
+      <br />
+      <ContactForm persons={persons} setPersons={setPersons}/>
+      <Contacts persons={persons} searchInput={searchInput}/>
     </div>
   )
 }
