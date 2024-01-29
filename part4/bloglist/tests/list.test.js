1afbd0b81072e46eb2fb36af068a89a1aa6c3337
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-01-29 19:12:53 +0100

    Exercise 4.4

--- part4/bloglist/tests/list.test.js
+++ part4/bloglist/tests/list.test.js
@@ -1,8 +1,49 @@
-const listHelper = require("../utils/list_helper").dummy
+const listHelper = require("../utils/list_helper")
 
 test("Dummy returns one", () => {
   const blogs = []
 
-  const result = listHelper(blogs)
+  const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe("Total likes", () => {
+  const blogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Man just put anything",
+      author: "Not me",
+      url: "https://example.com",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a669234d17f8",
+      title: "Doesn't really matter",
+      author: "Me",
+      url: "https://example.com",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "6b422aa71b54a676234d17f8",
+      title: "Does it now",
+      author: "Someone",
+      url: "https://google.com",
+      likes: 2,
+      __v: 1
+    }
+  ]
+
+  test("Of an empty list is 0", () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test("Of a list with only one blog is the same as the likes of said blog", () => {
+    expect(listHelper.totalLikes([blogs[0]])).toBe(5)
+  })
+
+  test("Of a bigger list is calculated correctly", () => {
+    expect(listHelper.totalLikes(blogs)).toBe(14)
+  })
 })
