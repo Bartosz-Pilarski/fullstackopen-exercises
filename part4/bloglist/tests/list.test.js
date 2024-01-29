beaa18700514888bf74b2db4b6da7f18856490ef
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-01-29 19:30:45 +0100

    Exercise 4.5

--- part4/bloglist/tests/list.test.js
+++ part4/bloglist/tests/list.test.js
@@ -7,34 +7,35 @@ test("Dummy returns one", () => {
   expect(result).toBe(1)
 })
 
-describe("Total likes", () => {
-  const blogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Man just put anything",
-      author: "Not me",
-      url: "https://example.com",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a669234d17f8",
-      title: "Doesn't really matter",
-      author: "Me",
-      url: "https://example.com",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "6b422aa71b54a676234d17f8",
-      title: "Does it now",
-      author: "Someone",
-      url: "https://google.com",
-      likes: 2,
-      __v: 1
-    }
-  ]
+//Shared between blog tests
+const blogs = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Man just put anything",
+    author: "Not me",
+    url: "https://example.com",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a669234d17f8",
+    title: "Doesn't really matter",
+    author: "Me",
+    url: "https://example.com",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "6b422aa71b54a676234d17f8",
+    title: "Does it now",
+    author: "Someone",
+    url: "https://google.com",
+    likes: 2,
+    __v: 1
+  }
+]
 
+describe("Total likes", () => {
   test("Of an empty list is 0", () => {
     expect(listHelper.totalLikes([])).toBe(0)
   })
@@ -45,5 +46,19 @@ describe("Total likes", () => {
 
   test("Of a bigger list is calculated correctly", () => {
     expect(listHelper.totalLikes(blogs)).toBe(14)
+  })
+})
+
+describe("Favorite blog", () => {
+  test("Of an empty list is an empty object", () => {
+    expect(listHelper.favoriteBlog([])).toEqual({})
+  })
+
+  test("Of a list with only one blog to be that blog", () => {
+    expect(listHelper.favoriteBlog([blogs[2]])).toEqual(blogs[2])
+  })
+
+  test("Of a bigger list to show the one with the highest likes value", () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[1])
   })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -9,7 +9,16 @@ const totalLikes = (blogs) => {
     : blogs.reduce((sum, current) => sum+current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) return {}
+
+  let max = blogs[0]
+  blogs.forEach((blog) => {if(blog.likes >= max.likes) max = blog})
+  return max
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
