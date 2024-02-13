a960e4ec3cde4efb245e99c9edcfe48432aafeae
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-02-13 19:32:37 +0100

    Exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import ToggleVisibility from "./ToggleVisibility"
+
+const Blog = ({ blog }) => {
+  const {title, author, url, likes} = blog
+  return(
+    <div style={{display: "inline-flex", flexDirection: "column", border: "2px solid black", margin: "0.2rem", padding: "0.25rem", textAlign: "center"}}>
+      <p>{title}</p>
+      <ToggleVisibility
+        buttonLabels={{
+          open: "View details",
+          close: "Hide details"
+        }}
+      >
+      <p>{author}</p>
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
+        <span>{likes}</span>
+        <button >like</button>
+      </div>
+
+      <p>{url}</p>
+      </ToggleVisibility>
+    </div>  
+  )
+}
 
 export default Blog
