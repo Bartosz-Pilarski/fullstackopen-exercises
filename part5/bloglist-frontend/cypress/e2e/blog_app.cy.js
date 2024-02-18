f1c184b55e7eac30a1d96850a8d67145fc7d8cca
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-02-18 22:00:12 +0100

    Exercise 5.19

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,13 +1,38 @@
+Cypress.Commands.add("login", ({ username, password }) => {
+  cy.request("POST", `${Cypress.env("BACKEND")}/login`, {
+    username,
+    password
+  }).then(response => {
+    localStorage.setItem("bloglistUser", JSON.stringify(response.body))
+    cy.visit("")
+  })
+})
+
+Cypress.Commands.add("addBlog", ([ title, author, url ]) => {
+  cy.request({
+    method: "POST", 
+    url: `${Cypress.env("BACKEND")}/blogs`,
+    body: {
+      title,
+      author,
+      url
+    },
+    headers: {
+      "Authorization": `Bearer ${JSON.parse(localStorage.getItem("bloglistUser")).token}`
+    }
+  })
+})
+
 describe("Blog app", function() {
   beforeEach(function() {
-    cy.request("POST", "http://localhost:3003/api/testing/reset")
+    cy.request("POST", `${Cypress.env("BACKEND")}/testing/reset`)
     const newUser = {
       username: "JohnFH",
       name: "Jonathan ForHonor",
       password: "thisgamesucks"
     }
-    cy.request("POST", "http://localhost:3003/api/users", newUser)
-    cy.visit("http://localhost:5173")
+    cy.request("POST", `${Cypress.env("BACKEND")}/users`, newUser)
+    cy.visit("")
   })
 
   it("Login form is shown by default", function() {
@@ -43,6 +68,35 @@ describe("Blog app", function() {
       cy.contains("Hello, Jonathan ForHonor")
       cy.contains("log out")
       cy.contains("Create new blog")
+    })
+
+    describe("When logged in", function() {
+      beforeEach(function() {
+        cy.login({
+          username: "JohnFH",
+          password: "thisgamesucks"
+        })
+      })
+      it.only("...user can create a new blog", function() {
+        cy.contains("Create new blog")
+          .click()
+
+        cy.get("input[placeholder=\"Name the blog\"]")
+          .type("For Honor sucks")
+
+        cy.get("input[placeholder=\"Name the author\"")
+          .type("Jonathan ForHonor")
+
+        cy.get("input[placeholder=\"blogurl.com\"")
+          .type("https://itruinedmylife.com")
+
+        cy.get("form")
+          .contains("button[type=\"submit\"]", "Create")
+          .click()
+
+        cy.contains("Blog For Honor sucks created succesfully")
+        cy.contains("For Honor sucks")
+      })
     })
   })
 })
