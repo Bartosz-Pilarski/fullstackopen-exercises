74f3e28839cecd0053c90e324f840e0289b05647
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-06-06 16:27:37 +0200

    Exercise 9.3

--- part9/exercises/bmiCalculator.ts
+++ part9/exercises/bmiCalculator.ts
@@ -1,24 +1,24 @@
-// interface BodyStats {
-//   height: number,
-//   weight: number
-// }
+interface BodyStats {
+  height: number,
+  weight: number
+}
 
 type BMIClass = 'underweight' | 'normal' | 'overweight';
 
-// const parseArguments = (args: string[]): BodyStats => {
-//   if(args.length < 4) throw new Error('Too little arguments');
-//   if(args.length > 4) throw new Error('Too many arguments');
+const parseArguments = (args: string[]): BodyStats => {
+  if(args.length < 4) throw new Error('Too little arguments');
+  if(args.length > 4) throw new Error('Too many arguments');
 
-//   const height = Number(args[2]);
-//   const weight = Number(args[3]);
+  const height = Number(args[2]);
+  const weight = Number(args[3]);
 
-//   if(isNaN(height) || isNaN(weight)) throw new Error('Provided values are not numbers');
+  if(isNaN(height) || isNaN(weight)) throw new Error('Provided values are not numbers');
 
-//   return {
-//     height,
-//     weight
-//   }
-// }
+  return {
+    height,
+    weight
+  }
+}
 
 const calculateBmi = (height: number, weight: number): BMIClass => {
   const heightToMeters = height/100
@@ -29,7 +29,8 @@ const calculateBmi = (height: number, weight: number): BMIClass => {
 }
 
 try {
-  console.log(calculateBmi(180, 74))
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
 } catch (error: unknown) {
   let errMsg = 'Mayday: ';
   if(error instanceof Error) errMsg += error.message;
--- part9/exercises/exerciseCalculator.ts
+++ part9/exercises/exerciseCalculator.ts
@@ -1,3 +1,20 @@
+interface ExerciseInput {
+  target: number,
+  days: number[]
+}
+
+const parseExerciseInput = (args: string[]): ExerciseInput => {
+  if(args.length < 4) throw new Error('Too few arguments')
+  
+  const target = Number(args[2])
+  const days = args.splice(3).map((day) => Number(day))
+
+  return {
+    target,
+    days
+  }
+}
+
 interface ExerciseData {
   periodLength: number,
   trainingDays: number,
@@ -16,19 +33,19 @@ interface RatingSet {
   ratingDesc: string
 }
 
-type Rating = 1 | 2 | 3
+type Rating = 1 | 2 | 3;
 
-const calculateAverage = (data: number[]): number => data.reduce((total, currentVal) => total+currentVal, 0)/data.length
+const calculateAverage = (data: number[]): number => data.reduce((total, currentVal) => total+currentVal, 0)/data.length;
 
 const calculateTrainingDays = (data: number[]): number => {
   let count = 0
-  data.forEach(day => { if(day > 0) count++ })
+  data.forEach(day => { if(day > 0) count++ });
 
   return count
 }
 
 const calculateRating = (average: number, target: number): RatingSet => {
-  const threshold = target*0.75
+  const threshold = target*0.75;
 
   if(average < threshold) return { rating: 1, ratingDesc: 'Poor showing, try harder!' }
   if(average < target) return { rating: 2, ratingDesc: 'Almost there, keep going!' }
@@ -36,8 +53,8 @@ const calculateRating = (average: number, target: number): RatingSet => {
 }
 
 const calculateExercises = (exerciseDays: number[], target: number): ExerciseData => {
-  let averageVal: number = calculateAverage(exerciseDays)
-  let ratingSet: RatingSet = calculateRating(averageVal, target)
+  let averageVal: number = calculateAverage(exerciseDays);
+  let ratingSet: RatingSet = calculateRating(averageVal, target);
   
   return {
     periodLength: exerciseDays.length,
@@ -53,4 +70,12 @@ const calculateExercises = (exerciseDays: number[], target: number): ExerciseDat
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const inputs: ExerciseInput = parseExerciseInput(process.argv);
+  console.log(calculateExercises(inputs.days, inputs.target));
+} catch (error: unknown) {
+  let msg = 'Mayday: ';
+  if(error instanceof Error) msg += error.message;
+  console.log(msg);
+}
+
