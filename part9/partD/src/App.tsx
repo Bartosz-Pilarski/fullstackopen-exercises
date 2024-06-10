3501dcb2186adcb28d198f8c092fb192ba75c677
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-06-10 21:12:27 +0200

    Exercise 9.15

--- part9/partD/src/App.tsx
+++ part9/partD/src/App.tsx
@@ -1,10 +1,34 @@
+import { assertNever } from "./util";
+
 interface HeaderProps {
   courseName: string
 }
 
-interface CoursePart {
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartRequirements;
+
+interface CoursePartBase {
   name: string,
   exerciseCount: number
+}
+interface CoursePartDescriptive extends CoursePartBase {
+  description: string;
+}
+
+
+interface CoursePartBasic extends CoursePartDescriptive {
+  kind: "basic"
+}
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+interface CoursePartBackground extends CoursePartDescriptive {
+  backgroundMaterial: string;
+  kind: "background"
+}
+interface CoursePartRequirements extends CoursePartDescriptive {
+  requirements: string[],
+  kind: "special"
 }
 
 interface ContentProps {
@@ -19,9 +43,46 @@ const Header = (props: HeaderProps): JSX.Element => {
   return <h1> {props.courseName} </h1>
 }
 
+const Part = (props: CoursePart) => {
+  switch (props.kind) {
+    case "basic":
+      return(
+        <div>
+          <b> {props.name} - {props.exerciseCount} exercises </b>
+          <p> {props.description} </p>
+        </div>
+      )
+    case "group":
+      return(
+        <div>
+          <b> {props.name} - {props.exerciseCount} exercises </b>
+          <p> Project exercises: {props.groupProjectCount} </p>
+        </div>
+      )
+    case "background":
+      return(
+        <div>
+          <b> {props.name} - {props.exerciseCount} exercises </b>
+          <p> {props.description} </p>
+          <p> Additional material: {props.backgroundMaterial} </p>
+        </div>
+      )
+    case "special":
+      return(
+        <div>
+        <b> {props.name} - {props.exerciseCount} exercises </b>
+        <p> {props.description} </p>
+        <p> Requirements: {props.requirements.join(', ')} </p>
+      </div>
+      )
+    default:
+      return assertNever(props)
+  }
+}
+
 const Content = (props: ContentProps): JSX.Element => {
   return <>
-    {props.courseParts.map((part: CoursePart) => (<p> {part.name} {part.exerciseCount} </p>))}
+    {props.courseParts.map((part: CoursePart, index) => (<Part key={index} {...part} />))}
   </>
 }
 
@@ -31,18 +92,44 @@ const Total = (props: TotalProps): JSX.Element => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
     }
   ];
 
